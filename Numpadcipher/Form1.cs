3fd4fabfd8871095eaeddf293bd0ba7cb5fb842a
Jordan <Jordancattwood@hotmail.com>
2018-06-18 21:50:34 +1000

    Init commit

---
+++ Numpadcipher/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Numpadcipher
+{
+    public partial class Form1 : Form
+    {
+
+        String inputString, outputString;
+        Convertholder converter = new Convertholder();
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void textBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                outputString = "=>";
+                inputString = textBox1.Text.ToLower();
+                inputString.ToCharArray();
+                foreach (char c in inputString)
+                {
+                    outputString += converter.Encode(c);
+
+                }
+
+                textBox2.AppendText(outputString + Environment.NewLine);
+
+                textBox1.Clear();
+            }
+        }
+        private void textBox3_KeyUp(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyCode == Keys.Enter)
+            {
+                //if(sender)
+                int x = 0, y = 0;
+
+                outputString = "=>";
+                inputString = textBox3.Text;
+                inputString.ToCharArray();
+                foreach (char c in inputString)
+                {
+                    if(c.ToString() != " ")
+                    {   
+                       x = inputString[0];
+                       y = inputString[1];
+                       outputString += converter.Decode(x, y);
+                    }
+                }
+
+                textBox4.AppendText(outputString + Environment.NewLine);
+               
+                textBox3.Clear();
+            }
+
+        }
+
+    }
+}
---
+++ Numpadcipher/convertholder.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numpadcipher
+{
+    public class Convertholder
+    {
+        string[,] ConvArr = new string[,]{
+                { "0"," ","@","$","_" },
+                { "1","/",".",",","?" },
+                { "2","a","b","c","!" },
+                { "3","d","e","f","_" },
+                { "4","g","h","i","_" },
+                { "5","j","k","l","_" },
+                { "6","m","n","o","_" },
+                { "7","p","q","r","s" },
+                { "8","t","u","v","_" },
+                { "9","w","x","y","z" },
+                };
+        public Convertholder()
+        {     
+        }
+
+        public string Encode(char s)
+        {
+            string x;
+            try
+            {
+                for (int i = 0; i < ConvArr.Length; i++)
+                {
+                    for (int ii = 0; ii <= 4; ii++)
+                    {
+                        if (s == Char.Parse(ConvArr[i, ii]))
+                        {
+                            return (i).ToString() + (ii).ToString() + " ";
+                        }
+
+                    }
+                }
+                return "cannot convert:" + s;
+
+            }
+            catch(Exception ex)
+            {
+               x = "(Major Fail finding:" + s+")";
+            }
+            return x;
+        }
+        public string Decode(int x, int y)
+        {
+            try
+            {
+                return ConvArr[x, y];
+            }
+            catch
+            {
+                return "invalid 2d co-ords:"+x+","+y;
+            }
+        }
+    }
+}
